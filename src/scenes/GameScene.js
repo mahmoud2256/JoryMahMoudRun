a57caa5b68b086c7f3e21379f36aaed1ff8929c4
Mahmoud Amin <mahmoud.asmaa@gmail.com>
2026-07-02 14:02:04 +0300

    fix game over buttons and translate to english

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -20,7 +20,8 @@ export default class GameScene extends Phaser.Scene {
         );
 
         this.background.setDisplaySize(width, height)
-
+        his.background.setDepth(0);
+        
         this.groundY = height - 110;
 
         this.groundStrip = this.add.tileSprite(
