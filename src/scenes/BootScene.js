409449661dbc54f2807fc066955dc8298232d44a
Mahmoud Amin <mahmoud.asmaa@gmail.com>
2026-07-02 15:19:24 +0300

    update

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -3,13 +3,8 @@ import Phaser from "phaser";
 export default class BootScene extends Phaser.Scene {
 
     preload() {
-
-        // Images فقط (ممنوع spritesheet عشان ما يبوظش)
         this.load.image("background", "assets/background.png");
         this.load.image("ground", "assets/ground.png");
-        this.load.image("coin", "assets/coin.png");
-        this.load.image("slime", "assets/slime.png");
-        this.load.image("barTexture", "assets/barTexture.png");
         this.load.image("playerRun", "assets/playerRun.png");
     }
 
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -7,19 +7,12 @@ export default class MenuScene extends Phaser.Scene {
         const w = this.scale.width;
         const h = this.scale.height;
 
-        this.add.text(w / 2, h / 2 - 80, "Jory Run", {
-            fontSize: "48px",
+        this.add.text(w / 2, h / 2, "PLAY", {
+            fontSize: "50px",
             color: "#ffffff"
-        }).setOrigin(0.5);
-
-        const playBtn = this.add.text(w / 2, h / 2 + 50, "PLAY", {
-            fontSize: "40px",
-            backgroundColor: "#00c853",
-            padding: { left: 20, right: 20, top: 10, bottom: 10 }
-        }).setOrigin(0.5).setInteractive();
-
-        playBtn.on("pointerdown", () => {
-            this.scene.start("GameScene");
-        });
+        }).setOrigin(0.5).setInteractive()
+          .on("pointerdown", () => {
+              this.scene.start("GameScene");
+          });
     }
 }
